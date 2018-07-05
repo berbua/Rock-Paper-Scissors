da73dace156f99bf20552f7da505d40382bd064c
Katarzyna Berbeka <katarzyna.berbeka@gmail.com>
2018-07-05 09:59:27 +0200

    js corrections - one round

--- script.js
+++ script.js
@@ -1,45 +1,56 @@
 console.log("Elczyk mordko, powodzonka");
 
 //one single round
-
-function computerPlay() {
-  let items = ["Rock", "Paper", "Scissors"];
+//compter choice
+const computerPlay = () => {
+  let items = ["rock", "paper", "scissors"];
   let randomItem = items[Math.floor(Math.random() * items.length)];
   return randomItem;
-}
+};
+//player choice
+/*let playerSelection = "Rock";
+playerSelection = playerSelection.toLowerCase();
+console.log(`Player choice: ${playerSelection}`);*/
 
+//one round
 function playRound(playerSelection, computerSelection) {
-  if (computerSelection == "rock" && playerSelection == "rock") {
-    return "The result is a tie!";
-  } else if (computerSelection == "rock" && playerSelection == "paper") {
-    return "You won! Paper beats rock!";
-  } else if (computerSelection == "rock" && playerSelection == "scissors") {
-    return "You loose! rock beats Scissors!";
+  if (playerSelection === computerSelection) {
+    return "Game is a tie!";
   }
-  if (computerSelection == "paper" && playerSelection === "rock") {
-    return "You loose! Paper beats rock!";
-  } else if (computerSelection == "paper" && playerSelection == "paper") {
-    return "The result is a tie!";
-  } else if (computerSelection == "paper" && playerSelection == "scissors") {
-    return "You won! Scissors beats paper!";
+  if (playerSelection === "rock") {
+    if (computerSelection === "paper") {
+      return "Computer wins";
+    } else {
+      return "User wins";
+    }
   }
-  if (computerSelection == "scissors" && playerSelection == "rock") {
-    return "You won! Rock beats Scissors!";
-  } else if (computerSelection == "scissors" && playerSelection == "paper") {
-    return "You loose Scissors beats paper";
-  } else if (computerSelection == "scissors" && playerSelection == "scissors") {
-    return "The result is a tie!";
+  if (playerSelection === "paper") {
+    if (computerSelection === "scissors") {
+      return "Computer wins";
+    } else {
+      return "User wins";
+    }
+  }
+  if (playerSelection === "scissors") {
+    if (computerSelection === "rock") {
+      return "Computer wins";
+    } else {
+      return "User wins";
+    }
   }
 }
 
-const playerSelection = prompt("Pick one: rock, Paper, Scissors");
 const computerSelection = computerPlay();
-//console.log("Computer: " + computerSelection);
-//console.log(playRound(playerSelection, computerSelection));
+console.log("Computer selection: " + computerSelection);
+
+const playerSelection = "rock";
+console.log(`User selection: ${playerSelection}`);
+
+console.log(playRound(playerSelection, computerSelection));
 
 // 5 rounds game
 
-function game(playerSelection, computerSelection) {
+/*function game(playerSelection, computerSelection) {
   for (i = 1; i < 3; i++) {
     alert("Round " + i);
     const playerSelection = prompt("Pick one: Rock, Paper, Scissors");
@@ -51,4 +62,4 @@ function game(playerSelection, computerSelection) {
   }
 }
 
-console.log(game(playerSelection, computerSelection));
+console.log(game(playerSelection, computerSelection));*/
